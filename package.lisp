da707871727c230f7b04ddb584805cdd839cead6
Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>
2013-06-15 19:23:24 +0200

    Added package.lisp file

---
+++ package.lisp
@@ -0,0 +1,14 @@
+
+(defpackage #:git-browser
+  (:use :cl :weblocks
+        :f-underscore :anaphora :wo-git :wo-graph :wo-graph-functions)
+  (:import-from :hunchentoot #:header-in
+		#:set-cookie #:set-cookie* #:cookie-in
+		#:user-agent #:referer)
+  (:documentation
+   "A web application based on Weblocks.")
+  (:export
+   #:read-graph
+   #:start-git-browser-server
+   #:stop-git-browser-server))
+
